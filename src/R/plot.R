fe6d7435b88703b0f787609ecbad99dc628d5f5f
David Mills <dave@debianvm.dvmlls.cat>
2015-05-05 15:13:07 -0400

    alter plot

--- src/R/plot.R
+++ src/R/plot.R
@@ -4,7 +4,8 @@ library(ggplot2)
 df <- read.table('output2.txt', skip=1, col.names=c('length', 'benchmark', 'impl', 'order', 'ns', 'v5')) %>%
   tbl_df %>% select(-v5)
 
-df %>% 
+plot <- df %>% 
   ggplot(aes(x=benchmark, y=log(ns), fill=impl)) + geom_bar(stat='identity', position='dodge') + xlab('') +
   facet_grid(length ~ order)
   
+ggsave('plot.png', plot, width=10, height=5, units="in", dpi=100)
