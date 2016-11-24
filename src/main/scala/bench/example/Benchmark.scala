9f26951117f53b6831391b99d4d8f98c7723c2f2
Ciarán Archer <carcher@zendesk.com>
2016-11-24 17:43:05 +0000

    Bench parsing AND validation.

--- src/main/scala/bench/example/Benchmark.scala
+++ src/main/scala/bench/example/Benchmark.scala
@@ -5,16 +5,8 @@ import java.util.Random
 import com.eclipsesource.schema.{SchemaType, SchemaValidator}
 import play.api.libs.json._
 import scala.io.Source
-import org.everit.json.schema.Schema
 import org.everit.json.schema.loader.SchemaLoader
 import org.json.JSONObject
-import org.json.JSONTokener
-
-class Sequences(isRandom:Boolean, length:Int)(implicit r:Random) {
-  def s = 0 until length
-  def ints = if (!isRandom) s else s.map(_ => r.nextInt()/2).distinct
-  def longs = if (!isRandom) s.map(_.toLong) else s.map(_ => r.nextLong()/2).distinct
-}
 
 // a caliper benchmark is a class that extends com.google.caliper.Benchmark
 // the SimpleScalaBenchmark trait does it and also adds some convenience functionality
@@ -22,8 +14,7 @@ class Benchmark extends SimpleScalaBenchmark {
   // to make your benchmark depend on one or more parameterized values, create fields with the name you want
   // the parameter to be known by, and add this annotation (see @Param javadocs for more details)
   // caliper will inject the respective value at runtime and make sure to run all combinations
-  //  @Param(Array("10", /* "100", "1000", "10000" */))
-  @Param(Array("10000"))
+  @Param(Array("10", "100", "1000", "10000"))
   val length: Int = 0
 
   // set up all your benchmark data here
@@ -36,7 +27,7 @@ class Benchmark extends SimpleScalaBenchmark {
   // you can use the 'repeat' method from the SimpleScalaBenchmark trait to repeat with relatively low overhead
   // however, if your code snippet is very fast you might want to implement the reps loop directly with 'while'
   def timePlaySchemaValidator(reps: Int) = repeat(reps) {
-    playSchemaValidator.validate
+    playSchemaValidator.parseAndValidate
   }
 
   def timeOrgJsonSchemaValidator(reps: Int) = repeat(reps) {
@@ -51,52 +42,45 @@ trait Logger {
 }
 
 class PlaySchemaValidator extends Logger {
-  /*
-   * Read the schema and create a validator
-   */
-  val jsonSchema = loadJson("page_view.schema.json")
+  val schemaData = loadDocument("page_view.schema.json")
+  val jsonSchema = Json.parse(schemaData)
   log("schema read successfully")
+
+  val documentData = loadDocument("page_view.sample.json")
+  log("document data read successfully")
 
   val schema = Json.fromJson[SchemaType](jsonSchema).get
   val validator = new SchemaValidator
 
-  /*
-   * Read the sample JSON document
-   */
-  val documentJson = loadJson("page_view.sample.json")
-  log("document read successfully")
-
-  def loadJson(resourceName: String) = {
+  def loadDocument(resourceName: String) = {
     val document = Source.fromInputStream(getClass.getResourceAsStream(resourceName))
-    val documentData = try document.mkString finally document.close()
-    Json.parse(documentData)
+    try document.mkString finally document.close()
   }
 
-
-  def validate = {
-    val result = validator.validate(schema, documentJson)
-    result match {
-      case _: JsError => println(s"\n\nDocument is invalid: ${result}\n")
-      case default => println("\n\nDocument is valid.\n")
-    }
-    result
+  def parseAndValidate = {
+    val documentJson = Json.parse(documentData)
+    validator.validate(schema, documentJson)
   }
 }
 
 class OrgJsonSchemaValidator extends Logger {
-  val schema = loadJson("page_view.schema.json")
+  val schemaData = loadDocument("page_view.schema.json")
+  val schema = new JSONObject(schemaData)
   log("schema read successfully")
-  val document = loadJson("page_view.sample.json")
-  log("document read successfully")
+
+  val documentData = loadDocument("page_view.sample.json")
+  println(s"****** >> ${documentData}")
+  log("document data read successfully")
 
   val validator = SchemaLoader.load(schema)
 
-  def loadJson(resourceName: String) = {
-    val document = new JSONTokener(getClass.getResourceAsStream(resourceName))
-    new JSONObject(document)
+  def loadDocument(resourceName: String) = {
+    Source.fromInputStream(getClass.getResourceAsStream(resourceName))
   }
 
   def validate = {
+    println(s"****** >> ${documentData}")
+    val document = new JSONObject(documentData)
     validator.validate(document)
     1 // Needs to return an int
   }
